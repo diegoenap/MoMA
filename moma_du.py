c8913abd873993e8bb9cd1c326a08248858ff3ee
Diego Enriquez <diegoenap@gmail.com>
2018-09-05 18:44:43 +1200

    Read GA data completing missing dates. Except for Channel info.

--- moma_du.py
+++ moma_du.py
@@ -13,7 +13,7 @@ moma_visitors.shape
 moma_visitors.head(10)
 moma_visitors.describe()
 
-# moma_visitors['Date'] = moma_visitors['Date'].astype('datetime64[ns]')
+#
 
 mf.plot_visitors(moma_visitors)
 
@@ -23,50 +23,77 @@ pandas.set_option('display.max_columns', 20)
 
 ga_bo_us = mf.read_ga_data('data/ga/moma_both_nony_nochn.tsv')
 
+ga_bo_test = mf.read_ga_data('data/ga/moma_both_all_nochn_test.tsv')
 
-GA_bo = mf.read_ga_data('data/ga/moma_both_all_nochn.tsv')
-GA_bo_os = mf.read_ga_data('data/ga/moma_both_nousa_nochn.tsv')
-GA_bo_us = mf.read_ga_data('data/ga/moma_both_usa_nochn.tsv')
-GA_bo_do = mf.read_ga_data('data/ga/moma_both_nony_nochn.tsv')
-GA_bo_lo = mf.read_ga_data('data/ga/moma_both_ny_nochn.tsv')
+ga_bo_test.head(10)
+ga_bo_test.tail(10)
 
-GA_bo_chn = mf.read_ga_data('data/ga/moma_both_all_chn.tsv', channels=True)
-GA_bo_chn_os = mf.read_ga_data('data/ga/moma_both_nousa_chn.tsv', channels=True)
-GA_bo_chn_us = mf.read_ga_data('data/ga/moma_both_usa_chn.tsv', channels=True)
-GA_bo_chn_do = mf.read_ga_data('data/ga/moma_both_nony_chn.tsv', channels=True)
-GA_bo_chn_lo = mf.read_ga_data('data/ga/moma_both_ny_chn.tsv', channels=True)
 
-GA_vi = mf.read_ga_data('data/ga/moma_visit_all_nochn.tsv')
-GA_vi_os = mf.read_ga_data('data/ga/moma_visit_nousa_nochn.tsv')
-GA_vi_us = mf.read_ga_data('data/ga/moma_visit_usa_nochn.tsv')
-GA_vi_do = mf.read_ga_data('data/ga/moma_visit_nony_nochn.tsv')
-GA_vi_lo = mf.read_ga_data('data/ga/moma_visit_ny_nochn.tsv')
 
-GA_vi_chn = mf.read_ga_data('data/ga/moma_visit_all_chn.tsv')
-GA_vi_chn_os = mf.read_ga_data('data/ga/moma_visit_nousa_chn.tsv')
-GA_vi_chn_us = mf.read_ga_data('data/ga/moma_visit_usa_chn.tsv')
-GA_vi_chn_do = mf.read_ga_data('data/ga/moma_visit_nony_chn.tsv')
-GA_vi_chn_lo = mf.read_ga_data('data/ga/moma_visit_ny_chn.tsv')
+ga_bo = mf.read_ga_data('data/ga/moma_both_all_nochn.tsv')
+ga_bo_os = mf.read_ga_data('data/ga/moma_both_nousa_nochn.tsv')
+ga_bo_us = mf.read_ga_data('data/ga/moma_both_usa_nochn.tsv')
+ga_bo_do = mf.read_ga_data('data/ga/moma_both_nony_nochn.tsv')
+ga_bo_lo = mf.read_ga_data('data/ga/moma_both_ny_nochn.tsv')
 
-GA_tk = mf.read_ga_data('data/ga/moma_tickets_all_nochn.tsv')
-GA_tk_os = mf.read_ga_data('data/ga/moma_tickets_nousa_nochn.tsv')
-GA_tk_us = mf.read_ga_data('data/ga/moma_tickets_usa_nochn.tsv')
-GA_tk_do = mf.read_ga_data('data/ga/moma_tickets_nony_nochn.tsv')
-GA_tk_lo = mf.read_ga_data('data/ga/moma_tickets_ny_nochn.tsv')
+ga_bo_chn = mf.read_ga_data('data/ga/moma_both_all_chn.tsv', channels=True)
+ga_bo_chn_os = mf.read_ga_data('data/ga/moma_both_nousa_chn.tsv', channels=True)
+ga_bo_chn_us = mf.read_ga_data('data/ga/moma_both_usa_chn.tsv', channels=True)
+ga_bo_chn_do = mf.read_ga_data('data/ga/moma_both_nony_chn.tsv', channels=True)
+ga_bo_chn_lo = mf.read_ga_data('data/ga/moma_both_ny_chn.tsv', channels=True)
 
-GA_tk_chn = mf.read_ga_data('data/ga/moma_tickets_all_chn.tsv')
-GA_tk_chn_os = mf.read_ga_data('data/ga/moma_tickets_nousa_chn.tsv')
-GA_tk_chn_us = mf.read_ga_data('data/ga/moma_tickets_usa_chn.tsv')
-GA_tk_chn_do = mf.read_ga_data('data/ga/moma_tickets_nony_chn.tsv')
-GA_tk_chn_lo = mf.read_ga_data('data/ga/moma_tickets_ny_chn.tsv')
+ga_bo_lo.head(10)
+
+
+ga_vi = mf.read_ga_data('data/ga/moma_visit_all_nochn.tsv')
+ga_vi_os = mf.read_ga_data('data/ga/moma_visit_nousa_nochn.tsv')
+ga_vi_us = mf.read_ga_data('data/ga/moma_visit_usa_nochn.tsv')
+ga_vi_do = mf.read_ga_data('data/ga/moma_visit_nony_nochn.tsv')
+ga_vi_lo = mf.read_ga_data('data/ga/moma_visit_ny_nochn.tsv')
+
+ga_vi_chn = mf.read_ga_data('data/ga/moma_visit_all_chn.tsv')
+ga_vi_chn_os = mf.read_ga_data('data/ga/moma_visit_nousa_chn.tsv')
+ga_vi_chn_us = mf.read_ga_data('data/ga/moma_visit_usa_chn.tsv')
+ga_vi_chn_do = mf.read_ga_data('data/ga/moma_visit_nony_chn.tsv')
+ga_vi_chn_lo = mf.read_ga_data('data/ga/moma_visit_ny_chn.tsv')
+
+ga_tk = mf.read_ga_data('data/ga/moma_tickets_all_nochn.tsv')
+ga_tk_os = mf.read_ga_data('data/ga/moma_tickets_nousa_nochn.tsv')
+ga_tk_us = mf.read_ga_data('data/ga/moma_tickets_usa_nochn.tsv')
+ga_tk_do = mf.read_ga_data('data/ga/moma_tickets_nony_nochn.tsv')
+ga_tk_lo = mf.read_ga_data('data/ga/moma_tickets_ny_nochn.tsv')
+
+ga_tk_chn = mf.read_ga_data('data/ga/moma_tickets_all_chn.tsv')
+ga_tk_chn_os = mf.read_ga_data('data/ga/moma_tickets_nousa_chn.tsv')
+ga_tk_chn_us = mf.read_ga_data('data/ga/moma_tickets_usa_chn.tsv')
+ga_tk_chn_do = mf.read_ga_data('data/ga/moma_tickets_nony_chn.tsv')
+ga_tk_chn_lo = mf.read_ga_data('data/ga/moma_tickets_ny_chn.tsv')
 
 
 
 ga_bo_us.columns
 ga_bo_us.shape
-ga_bo_us.tail(1)
+ga_bo_us.describe()
+ga_bo_us.info()
+ga_bo_us.head(10)
 
 mf.plot_gametrics(ga_bo_us, 'sessionsOld')
 
 
+ga_bo_us.loc[0, 'date2']
+
+
+d1 = datetime.datetime.strptime('20150701', '%Y%m%d')
+d2 = datetime.datetime.strptime('20150715', '%Y%m%d')
+step = datetime.timedelta(days=1)
+dateseq = []
+
+while d1 <= d2:
+    dateseq.append(d1)
+    #print(d1.strftime('%Y-%m-%d'))
+    d1 += step
+
+len(dateseq)
+
+dateseq[0].strftime('%Y-%m-%d')
 
--- moma_functions.py
+++ moma_functions.py
@@ -6,6 +6,51 @@ import datetime as dt
 # Read datasets functions
 
 
+def complete_dates(data, channels):
+    """Complete the missing dates with null values"""
+    # Sort the dataframe by date to get the initial and final date and the difference in days
+    data.sort_values(by='date2', ascending=True, inplace=True)
+    d1 = data.loc[0, 'date2']
+    d2 = data.loc[data.shape[0] - 1, 'date2']
+    diff = d2 - d1
+    if ~channels:
+        if data.shape[0] != diff.days + 1:
+            print('Some dates are missing!')
+            # Create a complete sequence of dates
+            step = dt.timedelta(days=1)
+            dateseq = []
+            while d1 <= d2:
+                dateseq.append(d1)
+                d1 += step
+            # Check for missing dates and create a temporal dataframe (tmpdf) with the missing rows, filled with 0's.
+            # Then, append tmpdf to the original dataset
+            i = 0
+            j = 0
+            tmpdf = pandas.DataFrame()
+            col_list = ['date', 'users', 'usersNew', 'percentNewSessions', 'sessions', 'bounceRate', 'avgSessionDuration',
+                        'pageviews', 'pageviewsPerSession', 'uniquePageviews', 'avgTimeOnPage', 'usersOld', 'sessionsNew',
+                        'sessionsOld', 'sessionsBounce', 'sessionsNoBounce', 'date2']
+            while j < len(dateseq):
+                if data.loc[i, 'date2'] == dateseq[j]:
+                    i += 1
+                    j += 1
+                else:
+                    # Add missing row
+                    missrow = pandas.DataFrame([[int(dateseq[j].strftime('%Y%m%d')), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, dateseq[j]]],
+                                               columns=col_list)
+                    tmpdf = tmpdf.append(missrow, ignore_index=True)
+                    j += 1
+            # Append the nissing rows to the original dataset and sort again
+            data = data.append(tmpdf, ignore_index=True)
+            return data.sort_values(by='date2', ascending=True)
+        else:
+            print('Complete dates')
+            return data
+    else:
+        print('Dataset with Channel info')
+        return data
+
+
 def read_ga_data(filename, channels=False):
     """Read Google Analytics datasets."""
     print('Reading dataset...')
@@ -22,6 +67,8 @@ def read_ga_data(filename, channels=False):
                          'ga:pageviewsPerSession': 'pageviewsPerSession',
                          'ga:uniquePageviews': 'uniquePageviews',
                          'ga:avgTimeOnPage': 'avgTimeOnPage'}, inplace=True)
+    if channels:
+        data.rename(columns={'ga:channelGrouping': 'channel'}, inplace=True)
     # Correct percentage
     data['percentNewSessions'] = data['percentNewSessions'] / 100
     data['bounceRate'] = data['bounceRate'] / 100
@@ -31,21 +78,17 @@ def read_ga_data(filename, channels=False):
     data['sessionsOld'] = data['sessions'] * (1 - data['percentNewSessions'])
     data['sessionsBounce'] = data['sessions'] * data['bounceRate']
     data['sessionsNoBounce'] = data['sessions'] * (1 - data['bounceRate'])
-    # Check complete range of days
-    d1 = dt.datetime.strptime(data.iloc[0, 0].astype('str'), '%Y%m%d')
-    d2 = dt.datetime.strptime(data.iloc[data.shape[0] - 1, 0].astype('str'), '%Y%m%d')
-    diff = d2 - d1
-    if data.shape[0] != diff.days + 1:
-        print('Some dates are missing!')
-    else:
-        print('Complete dates')
+    # Create date object column and sort the dataframe by date
+    data['date2'] = [dt.datetime.strptime(d, '%Y%m%d') for d in data['date'].astype('str')]
+    # Check and complete range of days if necessary.
+    data = complete_dates(data, channels)
     return data
 
 
 # Plot datasets functions
 
 def plot_visitors(dataset):
-    plt.figure(figsize=(10,4))
+    plt.figure(figsize=(10, 4))
     plt.plot_date(x=[mdates.strpdate2num('%d/%m/%y')(d) for d in dataset['Date']], y=dataset['visitors'], fmt='r-')
     plt.title('MoMA visitors')
     plt.ylabel('Visitors')
@@ -54,8 +97,10 @@ def plot_visitors(dataset):
 
 
 def plot_gametrics(dataset, metricname):
-    plt.figure(figsize=(10,4))
-    plt.plot_date(x=[mdates.strpdate2num('%Y%m%d')(d) for d in dataset['date'].astype('str')], y=dataset[metricname], fmt='b-')
+    plt.figure(figsize=(10, 4))
+    plt.plot_date(x=[mdates.strpdate2num('%Y%m%d')(d) for d in dataset['date'].astype('str')],
+                  y=dataset[metricname],
+                  fmt='b-')
     plt.title('MoMA GA metric')
     plt.ylabel(metricname)
     plt.grid(True)
