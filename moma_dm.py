8a22aafc592fd26eee4a89596f4bc48565d2bcc3
Diego Enriquez <diegoenap@gmail.com>
2018-10-03 23:02:05 +1300

    Completed function to read ga metrics into dataframe, with missing dates addition.

--- moma_dm.py
+++ moma_dm.py
@@ -5,17 +5,24 @@ import pandas
 def moma_analysis():
     # Read visitors dataset
     vsdf = mf.read_visitors_data('data/visits/MoMA_visitors.csv')
-    print(vsdf.shape)
+    gadf = mf.read_ga_data('data/ga/moma_both_all_nochn.tsv')
+    # Limit the range of dates in the datasets. If the Visitors start date is smaller than the GA metrics start date, or
+    # if it is between the 'maxlag' range, then use the GA metric start date. Otherwise the Initial date is the Visitors
+    # start date minus the 'maxlag'.
+    max_lag = pandas.Timedelta(days=31)
+    if (vsdf.index[0] - gadf.index[0]) <= max_lag:
+        init_date = gadf.index[0]
+    else:
+        init_date = vsdf.index[0] - max_lag.days
+    # For end_date use the smaller final date
+    end_date = min(vsdf.index[-1], gadf.index[-1])
+    # Limit the datasets with the new initial and end dates
+    vsdf = vsdf[init_date + max_lag:end_date]
+    gadf = gadf[str(init_date):str(end_date)]
 
-    gadf = mf.read_ga_data('data/ga/moma_both_all_chn.tsv', channels=True)
-    print(gadf.shape)
+    # Get correlation matrix
+    mf.get_correlation_matrix(vsdf, gadf)
 
-
-    # # Read GA metrics
-    # gadf = mf.read_ga_data('data/ga/moma_both_all_nochn.tsv')
-    #
-    # max_lag = pandas.Timedelta(days=31)
-    #
     # # Limit the datasets
     # if (vsdf.date2.iloc[0] - gadf.date2.iloc[0]) <= max_lag:
     #     # print('Distance between both init dates allow for using GA metric init date as the initial date')
--- moma_functions.py
+++ moma_functions.py
@@ -16,102 +16,103 @@ def read_visitors_data(filename):
     # return data.sort_values(by='date2', ascending=True).reset_index(drop=True)
 
 
-def complete_dates(data, channels):
-    """Complete the missing dates with null values"""
-    # Sort the dataframe by date to get the initial and final date and the difference in days
-    data.sort_values(by='date2', ascending=True, inplace=True)
-    d1 = data.loc[0, 'date2']
-    d2 = data.loc[data.shape[0] - 1, 'date2']
-    # diff is the number of dates between the start and the end dates of the dataset. If diff is greater than the
-    # number of rows it means that there are missing dates
-    diff = d2 - d1
-    if not channels:
-        if data.shape[0] != diff.days + 1:
-            print('Some dates are missing!')
-            # Create a complete sequence of dates
-            step = dt.timedelta(days=1)
-            dateseq = []
-            while d1 <= d2:
-                dateseq.append(d1)
-                d1 += step
-            # Check for missing dates and create a temporal dataframe (tmpdf) with the missing rows, filled with 0's.
-            # Then, append tmpdf to the original dataset
-            i = 0
-            j = 0
-            tmpdf = pd.DataFrame()
-            col_list = ['date', 'users', 'usersNew', 'percentNewSessions', 'sessions', 'bounceRate', 'avgSessionDuration',
-                        'pageviews', 'pageviewsPerSession', 'uniquePageviews', 'avgTimeOnPage', 'usersOld', 'sessionsNew',
-                        'sessionsOld', 'sessionsBounce', 'sessionsNoBounce', 'date2']
-            while j < len(dateseq):
-                if data.loc[i, 'date2'] == dateseq[j]:
-                    i += 1
-                    j += 1
-                else:
-                    # Add missing row
-                    missrow = pd.DataFrame([[int(dateseq[j].strftime('%Y%m%d')), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, dateseq[j]]],
-                                               columns=col_list)
-                    tmpdf = tmpdf.append(missrow, ignore_index=True)
-                    j += 1
-            # Append the nissing rows to the original dataset and sort again
-            data = data.append(tmpdf, ignore_index=True)
-            return data.sort_values(by='date2', ascending=True).reset_index(drop=True)
-        else:
-            print('Complete dates')
-            return data.reset_index(drop=True)
-    else:
-        print('Dataset with Channel info')
-        print(data.shape)
-        print(diff.days + 1)
-        data.sort_values(by=['date2', 'channel'], inplace=True)
-        channel_names = ['(Other)', 'Direct', 'Display', 'Email', 'Organic Search', 'Paid Search', 'Referral', 'Social']
-        if data.shape[0] != (diff.days + 1) * len(channel_names):
-            print('Some dates are missing!')
-            # Create a complete sequence of dates
-            step = dt.timedelta(days=1)
-            dateseq = []
-            while d1 <= d2:
-                dateseq.append(d1)
-                d1 += step
-            # Check for missing dates and create a temporal dataframe (tmpdf) with the missing rows, filled with 0's.
-            # Then, append tmpdf to the original dataset
-            i = 0
-            j = 0
-            tmpdf = pd.DataFrame()
-            col_list = ['date', 'channel', 'users', 'usersNew', 'percentNewSessions', 'sessions', 'bounceRate',
-                        'avgSessionDuration',
-                        'pageviews', 'pageviewsPerSession', 'uniquePageviews', 'avgTimeOnPage', 'usersOld',
-                        'sessionsNew',
-                        'sessionsOld', 'sessionsBounce', 'sessionsNoBounce', 'date2']
-            while j < len(dateseq):
-                k = 0
-                #print('Checking channels...')
-                while k < len(channel_names):
-                    if i < data.shape[0] and data.loc[i, 'date2'] == dateseq[j] and data.loc[i, 'channel'] == channel_names[k]:
-                        #print('Channel FOUND: {} i: {} k: {}'.format(channel_names[k], i, k))
-                        i += 1
-                        k += 1
-                    else:
-                        # Add missing row
-                        #print('Channel missing: {} i: {} k: {}'.format(channel_names[k], i, k))
-                        missrow = pd.DataFrame(
-                            [[int(dateseq[j].strftime('%Y%m%d')), channel_names[k], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                              0, 0, 0, 0, 0, dateseq[j]]],
-                            columns=col_list)
-                        tmpdf = tmpdf.append(missrow, ignore_index=True)
-                        k += 1
-                #print('Increase j')
-                j += 1
-            # Append the nissing rows to the original dataset and sort again
-            data = data.append(tmpdf, ignore_index=True)
-            return data.sort_values(by=['date2', 'channel'], ascending=True).reset_index(drop=True)
-        else:
-            print('Complete dates')
-            return data.reset_index(drop=True)
+# def complete_dates(data, channels):
+#     """Complete the missing dates with null values"""
+#     # Sort the dataframe by date to get the initial and final date and the difference in days
+#     data.sort_values(by='date2', ascending=True, inplace=True)
+#     d1 = data.loc[0, 'date2']
+#     d2 = data.loc[data.shape[0] - 1, 'date2']
+#     # diff is the number of dates between the start and the end dates of the dataset. If diff is greater than the
+#     # number of rows it means that there are missing dates
+#     diff = d2 - d1
+#     if not channels:
+#         if data.shape[0] != diff.days + 1:
+#             print('Some dates are missing!')
+#             # Create a complete sequence of dates
+#             step = dt.timedelta(days=1)
+#             dateseq = []
+#             while d1 <= d2:
+#                 dateseq.append(d1)
+#                 d1 += step
+#             # Check for missing dates and create a temporal dataframe (tmpdf) with the missing rows, filled with 0's.
+#             # Then, append tmpdf to the original dataset
+#             i = 0
+#             j = 0
+#             tmpdf = pd.DataFrame()
+#             col_list = ['date', 'users', 'usersNew', 'percentNewSessions', 'sessions', 'bounceRate', 'avgSessionDuration',
+#                         'pageviews', 'pageviewsPerSession', 'uniquePageviews', 'avgTimeOnPage', 'usersOld', 'sessionsNew',
+#                         'sessionsOld', 'sessionsBounce', 'sessionsNoBounce', 'date2']
+#             while j < len(dateseq):
+#                 if data.loc[i, 'date2'] == dateseq[j]:
+#                     i += 1
+#                     j += 1
+#                 else:
+#                     # Add missing row
+#                     missrow = pd.DataFrame([[int(dateseq[j].strftime('%Y%m%d')), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, dateseq[j]]],
+#                                                columns=col_list)
+#                     tmpdf = tmpdf.append(missrow, ignore_index=True)
+#                     j += 1
+#             # Append the nissing rows to the original dataset and sort again
+#             data = data.append(tmpdf, ignore_index=True)
+#             return data.sort_values(by='date2', ascending=True).reset_index(drop=True)
+#         else:
+#             print('Complete dates')
+#             return data.reset_index(drop=True)
+#     else:
+#         print('Dataset with Channel info')
+#         print(data.shape)
+#         print(diff.days + 1)
+#         data.sort_values(by=['date2', 'channel'], inplace=True)
+#         channel_names = ['(Other)', 'Direct', 'Display', 'Email', 'Organic Search', 'Paid Search', 'Referral', 'Social']
+#         if data.shape[0] != (diff.days + 1) * len(channel_names):
+#             print('Some dates are missing!')
+#             # Create a complete sequence of dates
+#             step = dt.timedelta(days=1)
+#             dateseq = []
+#             while d1 <= d2:
+#                 dateseq.append(d1)
+#                 d1 += step
+#             # Check for missing dates and create a temporal dataframe (tmpdf) with the missing rows, filled with 0's.
+#             # Then, append tmpdf to the original dataset
+#             i = 0
+#             j = 0
+#             tmpdf = pd.DataFrame()
+#             col_list = ['date', 'channel', 'users', 'usersNew', 'percentNewSessions', 'sessions', 'bounceRate',
+#                         'avgSessionDuration',
+#                         'pageviews', 'pageviewsPerSession', 'uniquePageviews', 'avgTimeOnPage', 'usersOld',
+#                         'sessionsNew',
+#                         'sessionsOld', 'sessionsBounce', 'sessionsNoBounce', 'date2']
+#             while j < len(dateseq):
+#                 k = 0
+#                 #print('Checking channels...')
+#                 while k < len(channel_names):
+#                     if i < data.shape[0] and data.loc[i, 'date2'] == dateseq[j] and data.loc[i, 'channel'] == channel_names[k]:
+#                         #print('Channel FOUND: {} i: {} k: {}'.format(channel_names[k], i, k))
+#                         i += 1
+#                         k += 1
+#                     else:
+#                         # Add missing row
+#                         #print('Channel missing: {} i: {} k: {}'.format(channel_names[k], i, k))
+#                         missrow = pd.DataFrame(
+#                             [[int(dateseq[j].strftime('%Y%m%d')), channel_names[k], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#                               0, 0, 0, 0, 0, dateseq[j]]],
+#                             columns=col_list)
+#                         tmpdf = tmpdf.append(missrow, ignore_index=True)
+#                         k += 1
+#                 #print('Increase j')
+#                 j += 1
+#             # Append the nissing rows to the original dataset and sort again
+#             data = data.append(tmpdf, ignore_index=True)
+#             return data.sort_values(by=['date2', 'channel'], ascending=True).reset_index(drop=True)
+#         else:
+#             print('Complete dates')
+#             return data.reset_index(drop=True)
 
 
-def read_ga_data(filename, channels=False):
+def read_ga_data(filename):
     """Read Google Analytics datasets."""
     data = pd.read_csv(filename, encoding='utf_16_le', sep='\t', parse_dates=['ga:date'], index_col='ga:date', dayfirst=True)
+    # Rename column names
     data.rename(columns={'ga:users': 'users',
                          'ga:newUsers': 'usersNew',
                          'ga:percentNewSessions': 'percentNewSessions',
@@ -122,9 +123,6 @@ def read_ga_data(filename, channels=False):
                          'ga:pageviewsPerSession': 'pageviewsPerSession',
                          'ga:uniquePageviews': 'uniquePageviews',
                          'ga:avgTimeOnPage': 'avgTimeOnPage'}, inplace=True)
-    if channels:
-        data.rename(columns={'ga:channelGrouping': 'channel'}, inplace=True)
-
     # Correct percentage
     data['percentNewSessions'] = data['percentNewSessions'] / 100
     data['bounceRate'] = data['bounceRate'] / 100
@@ -134,19 +132,36 @@ def read_ga_data(filename, channels=False):
     data['sessionsOld'] = data['sessions'] * (1 - data['percentNewSessions'])
     data['sessionsBounce'] = data['sessions'] * data['bounceRate']
     data['sessionsNoBounce'] = data['sessions'] * (1 - data['bounceRate'])
-
+    # Sort by date
+    data.sort_index(inplace=True)
     # Complete the dataset
-    if channels:
-        # Create dataframes for each channel, complete separatetly and the append in one final dataset
-        print('Complete dataset with channel info')
+    if 'ga:channelGrouping' in data.columns:
+        # For datasets with channel info
+        # Rename column
+        data.rename(columns={'ga:channelGrouping': 'channel'}, inplace=True)
+        # Create dataframes for each channel, complete separatetly and then append in one final dataset
         channels = ['(Other)', 'Direct', 'Display', 'Email', 'Organic Search', 'Paid Search', 'Referral', 'Social']
-        tmp = pd.DataFrame()
+        tmp = pd.DataFrame(columns=data.columns)
+        mindate = min(data.index)
+        maxdate = max(data.index)
         for ch in channels:
-            tmp1 = data[data.channel == ch]
-            print(tmp1.shape)
-            tmp2 = tmp1.asfreq('D', fill_value=0)
-            pd.concat([tmp, tmp2])
-            print(tmp.shape)
+            # Complete missing dates with 0s
+            tmp1 = data[data.channel == ch].asfreq('D', fill_value=0)
+            # If the temporal dataset has a start date bigger than the minimum, complete the dates
+            if tmp1.index[0] > mindate:
+                # Channel have missing dates at the beginning
+                tmpfill = pd.DataFrame(index=pd.date_range(start=mindate, end=(tmp1.index[0]-1), freq='D'), columns=data.columns)
+                tmpfill.fillna(0, inplace=True)
+                tmp1 = pd.concat([tmpfill, tmp1])
+                tmp1 = tmp1.asfreq('D')
+            # If the temporal dataset has an end date smaller than the maximum, complete the dates
+            if tmp1.index[-1] < maxdate:
+                # Channel have missing dates at the end
+                tmpfill = pd.DataFrame(index=pd.date_range(start=(tmp1.index[-1]+1), end=maxdate, freq='D'), columns=data.columns)
+                tmpfill.fillna(0, inplace=True)
+                tmp1 = pd.concat([tmp1, tmpfill])
+                tmp1 = tmp1.asfreq('D')
+            tmp = pd.concat([tmp, tmp1])
         data = tmp
     else:
         #
@@ -213,10 +228,11 @@ def get_correlation_matrix(vsdf, gadf):
     (from 0 to maxlag). It keeps the Visitors dataset fixed and try different lags going backwards with the GA metrics,
     that is why GA metrics dataset should be larger. The diference in day between both datasets is considered as the
     lag."""
-    a = vsdf.visitors
-    b = gadf.loc[:, 'users']
+    a = vsdf['2017-01'].visitors
+    b = gadf['2017-01'].users
     print(a.shape)
     print(b.shape)
+    print(a.corr(b, method='pearson'))
     #c = numpy.corrcoef(vsdf.visitors, gadf.loc[:, 'Users'])
     #return c
 
